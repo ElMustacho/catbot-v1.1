d9dc31fa4f51f614277ad578c045bc3b7d5bf92f
reddid <forgothowtoreddid@gmail.com>
2021-07-05 12:53:58 +0200

    Forgot a file

--- testing.py
+++ testing.py
@@ -1,9 +1,12 @@
-import os
-import shutil
+import requests
+import pandas as pd
 
-for root, dirs, files in os.walk('C:\\Users\\Fabrizio\\Desktop\\imgpics\\unit'):  # replace the . with your starting directory
-    for file in files:
-        path_file = os.path.join(root, file)
-        if path_file[-6:] == '00.png':
-            print(path_file)
-            shutil.copy(path_file, 'C:/Users/Fabrizio/Desktop/imgpics/pics/'+file)
+URL = 'https://battle-cats.fandom.com/wiki/Enemy_Release_Order'
+r = requests.get(URL)
+df_list = pd.read_html(r.text) # this parses all the tables in webpages to a list
+df = df_list[0]
+df.head()
+df = df['Enemy Release Order Table.1'].tolist()
+f = open("grabnamesenemies.txt", "a", encoding="utf-8")
+f.write(str(df[3:]))
+f.close()
